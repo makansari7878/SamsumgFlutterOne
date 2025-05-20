0055f58a5ecbae9dfe9c570c5e9570973f837a5a
kaleem <makanari.android@gmail.com>
2025-05-20 15:52:48 +0530

    Encryption

--- lib/screens/storage.dart
+++ lib/screens/storage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 class Storage extends StatefulWidget {
   const Storage({super.key});
@@ -9,9 +10,12 @@ class Storage extends StatefulWidget {
 }
 
 class _StorageState extends State<Storage> {
-
   TextEditingController usernameController = TextEditingController();
   String username = '';
+  final key = encrypt.Key.fromSecureRandom(32);
+  //final key1 = encrypt.Key.fromUtf8("input");
+  final iv = encrypt.IV.fromSecureRandom(16);
+  final encryption = encrypt.Encrypter(encrypt.AES(encrypt.Key.fromSecureRandom(32)));
 
   @override
   void initState() {
@@ -19,44 +23,58 @@ class _StorageState extends State<Storage> {
     loadSaveData();
   }
 
-
-  loadSaveData() async{
+  loadSaveData() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    setState(() {
-      username = sharedPreferences.getString("usernamekey") ?? "";
-    });
+    final encryptedUsername = sharedPreferences.getString("usernamekey");
+
+    if (encryptedUsername != null) {
+      final myEncrypted = encrypt.Encrypted.fromBase64(encryptedUsername);
+      final myDecrypted = encryption.decrypt(myEncrypted, iv: iv);
+      setState(() {
+        username = myDecrypted;
+      });
+    } else {
+      setState(() {
+        username = "";
+      });
+    }
   }
 
-  saveData() async{
+  saveData() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    final encrypted = encryption.encrypt(usernameController.text, iv: iv);
+    await sharedPreferences.setString("usernamekey", encrypted.base64);
     setState(() {
       username = usernameController.text;
     });
-    sharedPreferences.setString("usernamekey", "username");
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("STORAGE"),),
+      appBar: AppBar(
+        title: Text("STORAGE"),
+      ),
       body: Container(
-
         child: Column(
           children: [
-            SizedBox(height: 40,),
+            SizedBox(height: 40),
             Row(
               children: [
-                Expanded(child: TextField( controller: usernameController,)),
+                Expanded(
+                  child: TextField(controller: usernameController),
+                ),
               ],
             ),
-            SizedBox(height: 20,),
-            ElevatedButton(onPressed: (){
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
                 saveData();
-            }, child: Text("Submit")),
-            SizedBox(height: 40,),
+              },
+              child: Text("Submit"),
+            ),
+            SizedBox(height: 40),
             Text("Saved Data is : $username"),
-
           ],
         ),
       ),
